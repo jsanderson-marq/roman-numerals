33fe590fcb38202e561e2685042731fde96dd11e
Jeremy Sanderson <jsanderson@marq.com>
2026-01-08 14:38:04 -0700

    non-subtractive converter working

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,29 @@
-import { placeholder } from "@/index";
+import { convertArabicToRoman } from "@/index";
 
-describe("index", () => {
-  describe("when calling placeholder with input", () => {
-    it("should fail", () => {
-      expect(placeholder("input")).toBe("input");
-    });
+const testCases: { input: number; output: string }[] = [
+  { input: 0, output: "" },
+  { input: 1, output: "I" },
+  { input: 2, output: "II" },
+  { input: 3, output: "III" },
+  { input: 5, output: "V" },
+  { input: 6, output: "VI" },
+  { input: 7, output: "VII" },
+  { input: 8, output: "VIII" },
+  { input: 10, output: "X" },
+  { input: 11, output: "XI" },
+  { input: 36, output: "XXXVI" },
+  { input: 50, output: "L"},
+  { input: 67, output: "LXVII"},
+  { input: 100, output: "C"},
+  { input: 500, output: "D"},
+  { input: 1000, output: "M"},
+  { input: 1776, output: "MDCCLXXVI"},
+];
+
+describe("Work", () => {
+  describe("convertArabicToRoman", () => {
+    test.each(testCases)("$input should return $output", ({ input, output }) =>
+      expect(convertArabicToRoman(input)).toEqual(output)
+    );
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-export function placeholder(input: string): string {
-  return "";
+const mappings = [
+  { arabic: 1000, roman: "M" },
+  { arabic: 500, roman: "D" },
+  { arabic: 100, roman: "C" },
+  { arabic: 50, roman: "L" },
+  { arabic: 10, roman: "X" },
+  { arabic: 5, roman: "V" },
+  { arabic: 1, roman: "I" },
+];
+
+export function convertArabicToRoman(arabic: number): string {
+  if (arabic === 0) return "";
+
+  const mapped = mappings.find(p => p.arabic <= arabic);
+  return mapped!.roman + convertArabicToRoman(arabic - mapped!.arabic);
 }
